6a5bdfe1d77a1ed39a489d50090f91a048270f37
MattHughesEE <39420229+MattHughesEE@users.noreply.github.com>
2021-06-10 06:40:32 -0400

    Update deploy.sh

--- .semaphore/deploy.sh
+++ .semaphore/deploy.sh
@@ -8,7 +8,6 @@ stack_name=$2
 filename="ecom-$file_version.zip"
 
 cache restore gems-$SEMAPHORE_GIT_BRANCH-$(checksum Gemfile.lock),gems-develop,gems-master
-sem-version ruby 2.6.7
 bundle install --local --deployment --jobs=2 --path=vendor/bundle
 sem-service start postgres 10.6
 RAILS_ENV=production bundle exec rake db:create db:structure:load
