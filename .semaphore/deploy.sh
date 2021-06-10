158052e458931e4879ea6e1d0c59cb5236703338
MattHughesEE <39420229+MattHughesEE@users.noreply.github.com>
2021-06-10 06:51:10 -0400

    Update deploy.sh

--- .semaphore/deploy.sh
+++ .semaphore/deploy.sh
@@ -11,9 +11,9 @@ cache restore gems-$SEMAPHORE_GIT_BRANCH-$(checksum Gemfile.lock),gems-develop,g
 bundle install --local --deployment --jobs=2 --path=vendor/bundle
 sem-service start postgres 10.6
 RAILS_ENV=production bundle exec rake db:create db:structure:load
+SECRET_KEY_BASE=`bin/rake secret` bin/rake assets:precompile
 RAILS_ENV=production bundle exec rake assets:precompile
 
-RAILS_ENV=production SECRET_KEY_BASE=production_test_key rails c
 
 zip $file_name -9 -y -r . -x "spec/*" "tmp/*" "vendor/bundle/*" ".git/*"
 
